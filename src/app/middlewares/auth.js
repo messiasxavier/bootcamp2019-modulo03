0c06616886ca4e18309fa54210b277007f85c1e3
Messias Xavier <messiasxavier@MacBook-Air-de-Messias.local>
2019-11-11 14:22:26 -0300

    first commit

--- src/app/middlewares/auth.js
+++ src/app/middlewares/auth.js
@@ -14,9 +14,11 @@ export default async (req, res, next) => {
 
   try {
     const decoded = await promisify(jwt.verify)(token, authConfig.secret);
+
+    req.userId = decoded.id;
+
+    return next();
   } catch (err) {
     return res.status(401).json({ error: 'Token invalid' });
   }
-
-  return next();
 };
